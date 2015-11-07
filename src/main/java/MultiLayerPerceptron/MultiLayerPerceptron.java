c7bd7aefd9b2fef975bfbd6f50d4595df9fa62ba
Timothy Pratama <timothy.pratama@gmail.com>
2015-11-07 20:04:42 +0700

    override method untuk Multilayer Perceptron.

--- src/main/java/MultiLayerPerceptron/MultiLayerPerceptron.java
+++ src/main/java/MultiLayerPerceptron/MultiLayerPerceptron.java
@@ -1,6 +1,8 @@
 package MultiLayerPerceptron;
 
 import weka.classifiers.Classifier;
+import weka.core.Capabilities;
+import weka.core.Instance;
 import weka.core.Instances;
 
 /**
@@ -9,6 +11,15 @@ import weka.core.Instances;
 public class MultiLayerPerceptron extends Classifier {
     @Override
     public void buildClassifier(Instances data) throws Exception {
+    }
 
+    @Override
+    public Capabilities getCapabilities() {
+        return super.getCapabilities();
+    }
+
+    @Override
+    public double classifyInstance(Instance instance) throws Exception {
+        return super.classifyInstance(instance);
     }
 }

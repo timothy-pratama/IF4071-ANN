89f73f21481516020a3e1d9b58a3003a06870029
Timothy Pratama <timothy.pratama@gmail.com>
2015-11-27 23:12:03 +0700

    All algorithm verified with Excel.

--- src/main/java/DeltaTrainingRule/DeltaTrainingRule.java
+++ src/main/java/DeltaTrainingRule/DeltaTrainingRule.java
@@ -11,6 +11,7 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NominalToBinary;
+import weka.filters.unsupervised.attribute.Normalize;
 
 import java.io.Serializable;
 
@@ -23,6 +24,7 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
     private enum DeltaRuleType{Batch,Incremental};
     private DeltaRuleType rule = DeltaRuleType.Batch;
     private NominalToBinary nominalToBinaryFilter = new NominalToBinary();
+    private Normalize normalizeFilter = new Normalize();
 
     public DeltaTrainingRule(){
         topology = new Topology();
@@ -65,19 +67,19 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
         }
         nominalToBinaryFilter.setInputFormat(dataset);
         dataset = Filter.useFilter(dataset, nominalToBinaryFilter);
+        normalizeFilter.setInputFormat(dataset);
+        dataset =Filter.useFilter(dataset,normalizeFilter);
         topology.addInputLayer(dataset.numAttributes() - 1);
         topology.addOutputLayer(1);
 
         topology.connectAllNodes();
 
-        int epochError;
+        double epochError = 0;
         int tries = 0;
-        double previousdelta = 0.0;
         Node outputNode = topology.getOutputNode(0);
-        outputNode.setBiasWeight(0.0);
+        outputNode.setBiasWeight(0.1);
         double treshold = topology.isUseErrorThreshold()?topology.getEpochErrorThreshold():0.0;
         do {
-            
             epochError = 0;
             if(rule == DeltaRuleType.Incremental){
                 for (int i = 0; i < dataset.numInstances(); i++) {
@@ -85,46 +87,6 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
                     double target = instance.classValue();
                     topology.initInputNodes(instance);
 
-                    //topology.sortWeight(false, true);
-                    topology.resetNodesInput();
-
-                        for (int j = 0; j < topology.getWeights().size(); j++) {
-                            Weight weight = topology.getWeights().get(j);
-                            weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
-                        }
-                        outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
-                        double output = outputNode.getInput();
-                        for (int j = 0; j < topology.getWeights().size(); j++) {
-                            Weight weight = topology.getWeights().get(j);
-                            double delta = (topology.getLearningRate() * (target - output) * weight.getNode1().getOutput()) +
-                                    topology.getMomentumRate()* weight.getPreviousDeltaWeight();
-                            weight.setPreviousDeltaWeight(delta);
-                            weight.setWeight(weight.getWeight() + delta);
-                        }
-                        double biasWeight = outputNode.getBiasWeight();
-                        double delta = (topology.getLearningRate() * (target - output) * outputNode.getBiasValue()) +
-                                        topology.getMomentumRate()* outputNode.getPreviousDeltaWeight();
-                        outputNode.setPreviousDeltaWeight(delta);
-                        outputNode.setBiasWeight(biasWeight + delta);
-                        topology.resetNodesInput();
-                        for (int j = 0; j < topology.getWeights().size(); j++) {
-                            Weight weight = topology.getWeights().get(j);
-                            weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
-                        }
-                        outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
-                        output = outputNode.getInput();
-                        double squaredError = (output-target)*(output-target);
-                        epochError += squaredError;
-                    }
-                }
-            else if(rule == DeltaRuleType.Batch){
-                double delta = 0.0f;
-                for (int i = 0; i < dataset.numInstances(); i++) {
-                    Instance instance = dataset.instance(i);
-                    double target = instance.classValue();
-                    topology.initInputNodes(instance);
-
-                    //topology.sortWeight(false, true);
                     topology.resetNodesInput();
 
                     for (int j = 0; j < topology.getWeights().size(); j++) {
@@ -135,23 +97,70 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
                     double output = outputNode.getInput();
                     for (int j = 0; j < topology.getWeights().size(); j++) {
                         Weight weight = topology.getWeights().get(j);
-                        weight.setDeltabatch(weight.getDeltabatch() + (topology.getLearningRate() * (target - output) * weight.getNode1().getOutput()));
+                        double delta = (topology.getLearningRate() * (target - output) * weight.getNode1().getOutput()) +
+                                topology.getMomentumRate()* weight.getPreviousDeltaWeight();
+                        weight.setPreviousDeltaWeight(delta);
+                        weight.setWeight(weight.getWeight() + delta);
                     }
+                    double biasWeight = outputNode.getBiasWeight();
+                    double delta = (topology.getLearningRate() * (target - output) * outputNode.getBiasValue()) +
+                                    topology.getMomentumRate()* outputNode.getPreviousDeltaWeight();
+                    outputNode.setPreviousDeltaWeight(delta);
+                    outputNode.setBiasWeight(biasWeight + delta);
+                }
+                for(int i=0;i<dataset.numInstances();i++){
+                    Instance instance = dataset.instance(i);
+                    double target = instance.classValue();
+                    topology.initInputNodes(instance);
+                    topology.resetNodesInput();
+                    topology.initInputNodes(instance);
+                    for (int j = 0; j < topology.getWeights().size(); j++) {
+                        Weight weight = topology.getWeights().get(j);
+                        weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
+                    }
+                    outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
+                    double output = outputNode.getInput();
+                    double squaredError = (output-target)*(output-target);
+                    epochError += squaredError;
+                }
+            }
+            else if(rule == DeltaRuleType.Batch){
+                for (int i = 0; i < dataset.numInstances(); i++) {
+                    Instance instance = dataset.instance(i);
+                    double target = instance.classValue();
+                    topology.initInputNodes(instance);
+
+                    topology.resetNodesInput();
+                    for (int j = 0; j < topology.getWeights().size(); j++) {
+                        Weight weight = topology.getWeights().get(j);
+                        weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
+                    }
+                    outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
+                    double output = outputNode.getInput();
+                    for (int j = 0; j < topology.getWeights().size(); j++) {
+                        Weight weight = topology.getWeights().get(j);
+                        weight.setDeltabatch(weight.getDeltabatch() + topology.getLearningRate() * (target - output) * weight.getNode1().getOutput());
+                    }
+
                     outputNode.setDeltabatch(outputNode.getDeltabatch() + (topology.getLearningRate() * (target - output) * outputNode.getBiasValue()));
-                        
-                }               
+                }
+
                 for (int j = 0; j < topology.getWeights().size(); j++) {
                     Weight weight = topology.getWeights().get(j);
                     weight.setWeight(weight.getWeight()+weight.getDeltabatch() + topology.getMomentumRate()* weight.getPreviousDeltaWeight());
                     weight.setPreviousDeltaWeight(weight.getDeltabatch() + topology.getMomentumRate()* weight.getPreviousDeltaWeight());
                     weight.setDeltabatch(0.0);
                 }
-                outputNode.setBiasWeight(outputNode.getBiasWeight() + outputNode.getDeltabatch() + topology.getMomentumRate()* outputNode.getPreviousDeltaWeight());
+
+                outputNode.setBiasWeight(outputNode.getBiasWeight() + outputNode.getDeltabatch() + topology.getMomentumRate() * outputNode.getPreviousDeltaWeight());
                 outputNode.setPreviousDeltaWeight(outputNode.getDeltabatch() + topology.getMomentumRate()* outputNode.getPreviousDeltaWeight());
                 outputNode.setDeltabatch(0.0);
-                
+
+
+                //Squared n Eppoch Error
                 for (int i = 0; i < dataset.numInstances(); i++) {
                     Instance instance = dataset.instance(i);
+                    topology.initInputNodes(instance);
                     double target = instance.classValue();
                     double output = 0.0;
                     topology.resetNodesInput();
@@ -163,17 +172,42 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
                     output = outputNode.getInput();
                     double squaredError = (output-target)*(output-target);
                     epochError += squaredError;
+                    topology.resetNodesInput();
                 }
             }
             else{
                     throw (new Exception("Ruletype can only be batch or incremental!"));
             }
             tries++;
-            epochError *= 0.5f;
-            System.out.println("Iteration "+tries+" : "+epochError);
+            epochError *= 0.5;
         }
         while((epochError > treshold) && (!topology.isUseIteration() || (tries < topology.getNumIterations())));
 
+    }
+
+    @Override
+    public double classifyInstance(Instance data) throws Exception {
+        Instance instance = new Instance(data);
+        nominalToBinaryFilter.input(instance);
+        instance = nominalToBinaryFilter.output();
+        normalizeFilter.input(instance);
+        instance = normalizeFilter.output();
+
+        topology.initInputNodes(instance);
+        Node outputNode = topology.getOutputNode(0);
+        topology.resetNodesInput();
+
+        for (int j = 0; j < topology.getWeights().size(); j++) {
+            Weight weight = topology.getWeights().get(j);
+            weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
+        }
+        outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
+        
+        long output = Math.round(outputNode.getInput());
+        if(output > dataset.numClasses() - 1){
+            output = dataset.numClasses() - 1;
+        }
+        return (double)output;
     }
 
     @Override
@@ -196,23 +230,6 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
     }
 
     @Override
-    public double classifyInstance(Instance instance) throws Exception {
-        topology.initInputNodes(instance);
-        Node outputNode = topology.getOutputNode(0);
-        topology.resetNodesInput();
-
-        for (int j = 0; j < topology.getWeights().size(); j++) {
-            Weight weight = topology.getWeights().get(j);
-            weight.getNode2().setInput(weight.getNode2().getInput() + (weight.getNode1().getOutput() * weight.getWeight()));
-        }
-        outputNode.setInput(outputNode.getInput() + (outputNode.getBiasValue() * outputNode.getBiasWeight()));
-        
-        long output = Math.round(outputNode.getInput());
-        System.out.println("Double : "+ output);
-        return (double)output;
-    }
-
-    @Override
     public Capabilities getCapabilities() {
         Capabilities result = super.getCapabilities();
         result.disableAll();
@@ -232,16 +249,17 @@ public class DeltaTrainingRule extends Classifier implements Serializable{
         topology.setInitialWeight(0.0);
         topology.setMomentumRate(0.0);
         topology.setEpochErrorThreshold(0);
-        topology.setNumIterations(10000);
+        topology.setUseErrorThreshold(true);
+        topology.setUseIteration(true);
+        topology.setNumIterations(5);
 
         DeltaTrainingRule dtr = new DeltaTrainingRule(topology);
-        dtr.setDeltaRuleType(DeltaRuleType.Incremental);
+        dtr.setDeltaRuleType(DeltaRuleType.Batch);
         dtr.buildClassifier(dataset);
 
         Evaluation eval = Util.evaluateModel(dtr, dataset);
-        System.out.println(eval.toMatrixString());
         System.out.println(eval.toSummaryString());
-
+        System.out.println(eval.toMatrixString());
         System.out.println(dtr);
     }
 }
